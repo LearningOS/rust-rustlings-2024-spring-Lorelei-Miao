a3f9debc21d415ed2b3348a300ab55a4465f450e
lorelei-Miao <2132714883@qq.com>
2024-05-13 04:27:41 +0800

    update

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -7,36 +7,28 @@
 
 // This function returns how much icecream there is left in the fridge.
 // If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
-// all, so there'll be no more left :(
-    fn maybe_icecream(time_of_day: u16) -> Option<u16> {
-        if time_of_day >= 0 && time_of_day <= 23 {
-            if time_of_day >= 10 && time_of_day <= 22 {
-                Some(5)
-            } else {
-                Some(0)
-            }
-        } else {
-            None
-        }
+// all, so there'll be no more left 
+
+mod delicious_snacks {
+    // TODO: Fix these use statements
+    pub use self::fruits::{PEAR, APPLE};
+    pub use self::veggies::{CUCUMBER, CARROT};
+
+    mod fruits {
+        pub const PEAR: &'static str = "Pear";
+        pub const APPLE: &'static str = "Apple";
     }
-    
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-    
-        #[test]
-        fn check_icecream() {
-            assert_eq!(maybe_icecream(9), Some(0));
-            assert_eq!(maybe_icecream(10), Some(5));
-            assert_eq!(maybe_icecream(23), Some(0));
-            assert_eq!(maybe_icecream(22), Some(5));
-            assert_eq!(maybe_icecream(25), None);
-        }
-    
-        #[test]
-        fn raw_value() {
-            let icecreams = maybe_icecream(12);
-            assert_eq!(icecreams.unwrap(), 5);
-        }
+
+    mod veggies {
+        pub const CUCUMBER: &'static str = "Cucumber";
+        pub const CARROT: &'static str = "Carrot";
     }
-    
+}
+
+fn main() {
+    println!(
+        "favorite snacks: {} and {}",
+        delicious_snacks::PEAR,
+        delicious_snacks::CUCUMBER
+    );
+}
